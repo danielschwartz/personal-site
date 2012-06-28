5ce11e57ea05e796e4e6f85ce4c0da5a1dded6ac
Daniel Schwartz <dschwartz88@gmail.com>
2012-06-28 17:21:39 -0400

    adding mixpanel

--- index.php
+++ index.php
@@ -6,6 +6,34 @@
     <title>Daniel Schwartz</title>
 	<link rel=StyleSheet href="reset.css" type="text/css">
 	<link rel=StyleSheet href="global.css" type="text/css">
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <!-- start Mixpanel --><script type="text/javascript">(function(c,b){var a,d,h,e;a=c.createElement("script");a.type="text/javascript";a.async=!0;a.src=("https:"===c.location.protocol?"https:":"http:")+'//api.mixpanel.com/site_media/js/api/mixpanel.2.js';d=c.getElementsByTagName("script")[0];d.parentNode.insertBefore(a,d);b._i=[];b.init=function(a,c,f){function d(a,b){var c=b.split(".");2==c.length&&(a=a[c[0]],b=c[1]);a[b]=function(){a.push([b].concat(Array.prototype.slice.call(arguments,0)))}}var g=b;"undefined"!==typeof f?g=
+b[f]=[]:f="mixpanel";g.people=g.people||[];h="disable track track_pageview track_links track_forms register register_once unregister identify name_tag set_config people.set people.increment".split(" ");for(e=0;e<h.length;e++)d(g,h[e]);b._i.push([a,c,f])};window.mixpanel=b})(document,window.mixpanel||[]);
+mixpanel.init("3968295d4a53c81dbe093b7d3eb4b99e");</script><!-- end Mixpanel -->
+    <script type="text/javascript">
+        $(document).ready(function(){
+            mixpanel.track('HP-View');
+
+            var windowHeight = window.innerHeight,
+				heroHeight = $("#hero").height();
+				
+			$("#hero").css('margin-top', (windowHeight - heroHeight) / 3);
+			
+			$(".projects").click(function(e) {
+				e.preventDefault();
+				var projectList = $("#projects");
+				
+				if(projectList.is(":hidden")){
+					$(this).addClass("active");
+					projectList.fadeIn('slow');
+				}
+				else{
+					$(this).removeClass("active");
+					projectList.fadeOut('slow');
+				}
+			});
+        });
+    </script>
   </head>
   <body>
 	<div class="wrapper">
@@ -26,26 +54,5 @@
 			<span>Eventually.</span>
 		</div>
 	</div>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
-	<script type="text/javascript">
-		var windowHeight = window.innerHeight,
-			heroHeight = $("#hero").height();
-			
-		$("#hero").css('margin-top', (windowHeight - heroHeight) / 3);
-		
-		$(".projects").click(function(e) {
-			e.preventDefault();
-			var projectList = $("#projects");
-			
-			if(projectList.is(":hidden")){
-				$(this).addClass("active");
-				projectList.fadeIn('slow');
-			}
-			else{
-				$(this).removeClass("active");
-				projectList.fadeOut('slow');
-			}
-		});
-	</script>
   </body>
 </html>
